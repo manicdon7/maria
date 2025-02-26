ced3dfc6355a172e06d2b84cfbdbc1675eed684a
manicdon7 <manicdon7@gmail.com>
2025-02-26 23:04:17 +0530

    add tailwind-scrollbar plugin and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
 })
